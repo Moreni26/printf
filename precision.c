e49049583c163ce440ba0302c6189584883e7f55
Moreni26 <ayomide2605@gmail.com>
2023-07-24 05:01:28 -0700

    edited due to some failing checks

--- precision.c
+++ precision.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * get_precision -  function that gets the precision from the format string
  * @s:  the format string parameter
@@ -7,24 +6,26 @@
  * @ap: paramater for the argument pointer
  * Return: new pointer
 */
-
 char *get_precision(char *s, params_t *params, va_list ap)
 {
 	int d = 0;
 
 	if (*s != '.')
 		return (s);
-	s++;
+
+	s++; /* Move past the period '.' */
+
 	if (*s == '*')
 	{
 		d = va_arg(ap, int);
-		s++;
+		s++; /* Move past the asterisk '*' */
 	}
 	else
 	{
 		while (_isdigit(*s))
 			d = d * 10 + (*s++ - '0');
 	}
+
 	params->precision = d;
 	return (s);
 }
