f82813d665120eef6cba60b3653cac8ad1f1c192
Drpmartins <abonyiizuchukwu@gmail.com>
2023-07-25 00:53:08 -0700

    printf

--- precision.c
+++ precision.c
@@ -1,40 +1,40 @@
 #include "main.h"
 /**
- * get_precision -  function that gets the precision from the format string
- * @s:  the format string parameter
- * @params: the parameters struct
- * @ap: paramater for the argument pointer
- * Return: new pointer
+* get_precision -  function that gets the precision from the format string
+* @format: the format string parameter
+* @params: the parameters struct
+* @ap: parameter for the argument pointer
+* Return: new pointer
 */
 
-char *get_precision(char *s, params_t *params, va_list ap)
+int get_precision(char *format, params_t *params, va_list ap)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+int curr_i = *i + 1;
+int precision = -1;
 
-	if (format[curr_i] != '.')
-		return (precision);
+if (format[curr_i] != '.')
+return (precision);
 
-	precision = 0;
+precision = 0;
 
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
+for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+{
+if (is_digit(format[curr_i]))
+{
+precision *= 10;
+precision += format[curr_i] - '0';
+}
+else if (format[curr_i] == '*')
+{
+curr_i++;
+precision = va_arg(ap, int);
+break;
+}
+else
+break;
+}
 
-	*i = curr_i - 1;
+*i = curr_i - 1;
 
-	return (precision);
+return (precision);
 }
