8c6a253311a0ae5fac4c2aa8996f51a58fa05366
Drpmartins <abonyiizuchukwu@gmail.com>
2023-07-25 03:19:10 -0700

    printf

--- precision.c
+++ precision.c
@@ -1,42 +1,28 @@
-#includ#include "main.h"
-#include <ctype.h>
-
+#include "main.h"
 /**
- * get_precision -  function that gets the precision from the format string
- * @format:  the format string parameter
- * @params: the parameters struct
- * @ap: parameter for the argument pointer
+ * get_precision - A function that gets the precision from the format string
+ * @s: the format string
+ * @params: parameters struct
+ * @ap: paramater for the argument pointer
  * Return: new pointer
- */
-
-char *get_precision(char *format, params_t *params, va_list ap)
+*/
+char *get_precision(char *s, params_t *params, va_list ap)
 {
-    int curr_i = 0; /* Initialize the variable `i`*/
-    int precision = -1;
+	int d = 0;
 
-    if (format[curr_i] != '.')
-        return (NULL); /* Return NULL instead of precision */
-
-    precision = 0;
-
-    for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-    {
-        if (isdigit(format[curr_i]))/*Use isdigit from ctype.h*/
-        {
-            precision *= 10;
-            precision += format[curr_i] - '0';
-        }
-        else if (format[curr_i] == '*')
-        {
-            curr_i++;
-            precision = va_arg(ap, int);
-            break;
-        }
-        else
-            break;
-    }
-
-    /* No need to set `i` as it is not used in this function */
-
-    return (format);
+	if (*s != '.')
+		return (s);
+	s++;
+	if (*s == '*')
+	{
+		d = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+	params->precision = d;
+	return (s);
 }
